1cf6b350a5de88c29f4f6a5337be656f44a2bc08
dk1a <dk1a@protonmail.com>
2023-02-03 21:38:12 +0300

    return loadingState

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,6 @@
 import { SyncState } from "@latticexyz/network";
 import { useComponentValue } from "@latticexyz/react";
-import { Home } from "./Home";
+import { Navbar } from "./components/Navbar";
 import { useMUD } from "./mud/MUDContext";
 
 export const App = () => {
@@ -19,7 +19,14 @@ export const App = () => {
   return (
     <div className="w-screen flex h-screen items-center
     flex-col bg-gradient-to-br from-black via-cyan-900 to-black">
-      <Home/>
+
+      {loadingState.state !== SyncState.LIVE ? (
+        <div>
+          {loadingState.msg} ({Math.floor(loadingState.percentage)}%)
+        </div>
+      ) : (
+        <Navbar />
+      )}
     </div>
   );
 };
