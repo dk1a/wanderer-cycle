5111666b7cece5cc30f7e3008894e54337bb14a0
dk1a <dk1a@protonmail.com>
2023-02-07 19:45:45 +0300

    feat(client): add experience hook

---
+++ packages/client/src/mud/hooks/useExperience.ts
@@ -0,0 +1,18 @@
+import { useComponentValue } from "@latticexyz/react";
+import { EntityIndex } from "@latticexyz/recs";
+import { useMUD } from "../MUDContext";
+
+export const useExperience = (entity: EntityIndex) => {
+  const {
+    components: { Experience },
+  } = useMUD();
+
+  const experience = useComponentValue(Experience, entity);
+  if (!experience) return;
+
+  return {
+    strength: experience.strength,
+    arcana: experience.arcana,
+    dexterity: experience.dexterity,
+  };
+};
