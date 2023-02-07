1d4c979fabef67963d7a7a5ff7a7e70474746622
dk1a <dk1a@protonmail.com>
2023-02-07 13:47:23 +0300

    feat(client): add cycle to WandererContext, fix formatting

--- packages/client/src/contexts/WandererContext.tsx
+++ packages/client/src/contexts/WandererContext.tsx
@@ -1,11 +1,15 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import { useComponentValue } from "@latticexyz/react";
+import { EntityIndex } from "@latticexyz/recs";
+import { createContext, ReactNode, useContext, useMemo, useState } from "react";
+import { useMUD } from "../mud/MUDContext";
 
 type WandererContextType = {
-  selectedWandererEntity?: string,
-  selectWandererEntity: (wanderer: string) => void,
+  selectedWandererEntity?: EntityIndex;
+  selectWandererEntity: (wanderer: EntityIndex | undefined) => void;
 };
 
 const WandererContext = createContext<WandererContextType>({
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   selectWandererEntity: () => {},
 });
 
@@ -13,11 +17,22 @@ export const WandererProvider = (props: { children: ReactNode }) => {
   const currentValue = useContext(WandererContext);
   if (currentValue) throw new Error("WandererProvider can only be used once");
 
-  const [selectedWandererEntity, selectWandererEntity] = useState<string>()
+  const [selectedWandererEntity, selectWandererEntity] = useState<EntityIndex>();
+  const {
+    world,
+    components: { ActiveCycle },
+  } = useMUD();
+
+  const activeCycle = useComponentValue(ActiveCycle, selectedWandererEntity);
+  const cycleEntity = useMemo(() => {
+    return activeCycle?.value ? world.entityToIndex.get(activeCycle.value) : undefined;
+  }, [activeCycle, world]);
 
   const value = {
-    selectedWandererEntity, selectWandererEntity,
-  }
+    selectedWandererEntity,
+    selectWandererEntity,
+    cycleEntity,
+  };
   return <WandererContext.Provider value={value}>{props.children}</WandererContext.Provider>;
 };
 
