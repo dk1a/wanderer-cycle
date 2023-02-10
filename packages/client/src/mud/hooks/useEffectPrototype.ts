48f58510f1d88bc6007c24931970520727d2af03
dk1a <dk1a@protonmail.com>
2023-02-10 14:49:00 +0300

    feat(client): add useActiveEffect, fix useStatmodPrototype filename

--- packages/client/src/mud/hooks/useEffectPrototype.ts
+++ packages/client/src/mud/hooks/useEffectPrototype.ts
@@ -1,6 +1,7 @@
 import { useComponentValue } from "@latticexyz/react";
 import { EntityIndex } from "@latticexyz/recs";
 import { useMemo } from "react";
+import { defineEffectComponent } from "../components/EffectComponent";
 import { useMUD } from "../MUDContext";
 import { parseEffectStatmods } from "../utils/effectStatmod";
 
@@ -10,13 +11,10 @@ export enum EffectRemovability {
   PERSISTENT,
 }
 
-export const useEffectPrototype = (entity: EntityIndex) => {
-  const {
-    world,
-    components: { EffectPrototype },
-  } = useMUD();
+const useEffectPrototypeGeneric = (component: ReturnType<typeof defineEffectComponent>, entity: EntityIndex) => {
+  const { world } = useMUD();
 
-  const effectPrototype = useComponentValue(EffectPrototype, entity);
+  const effectPrototype = useComponentValue(component, entity);
 
   const statmods = useMemo(() => {
     if (!effectPrototype) return;
@@ -29,4 +27,20 @@ export const useEffectPrototype = (entity: EntityIndex) => {
     removability: effectPrototype.removability as EffectRemovability,
     statmods,
   };
+};
+
+export const useEffectPrototype = (entity: EntityIndex) => {
+  const {
+    components: { EffectPrototype },
+  } = useMUD();
+
+  return useEffectPrototypeGeneric(EffectPrototype, entity);
+};
+
+export const useAppliedEffect = (entity: EntityIndex) => {
+  const {
+    components: { AppliedEffect },
+  } = useMUD();
+
+  return useEffectPrototypeGeneric(AppliedEffect, entity);
 };
