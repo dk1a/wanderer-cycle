7f820311c70ded99fd6f8c104f86c9e90daa57a6
dk1a <dk1a@protonmail.com>
2023-02-16 00:57:33 +0300

    fix(client): fix ClaimTurnsButton

--- packages/client/src/components/info/ClaimTurnsButton.tsx
+++ packages/client/src/components/info/ClaimTurnsButton.tsx
@@ -1,50 +1,32 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useState } from "react";
 import { useWandererContext } from "../../contexts/WandererContext";
 import { useMUD } from "../../mud/MUDContext";
-import { EntityIndex } from "@latticexyz/recs";
 import CustomButton from "../UI/CustomButton/CustomButton";
 
-type ClaimTurnsButtonProps = {
-  turns: number;
-};
-
-export default function ClaimTurnsButton({ turns }: ClaimTurnsButtonProps) {
+export default function ClaimTurnsButton() {
   const { systems } = useMUD();
   const { selectedWandererEntity } = useWandererContext();
   // TODO add real claimable turns
   const claimableTurns = 10;
 
-  const isAvailable = useMemo(() => {
-    // TODO use proper availability
-    return true;
-  }, []);
-
-  const isWriterEnabled = useMemo(() => {
-    // TODO use proper availability
-    return true;
-  }, []);
-
   const [isBusy, setIsBusy] = useState(false);
 
-  const claimTurns = useCallback(
-    async (wandererEntity: EntityIndex) => {
-      setIsBusy(true);
-      const tx = await systems["system.ClaimCycleTurns"].executeTyped(wandererEntity);
-      await tx.wait();
-      setIsBusy(false);
-    },
-    [systems]
-  );
+  const claimTurns = useCallback(async () => {
+    if (!selectedWandererEntity) {
+      throw new Error("No wanderer entity selected");
+    }
+    setIsBusy(true);
+    const tx = await systems["system.ClaimCycleTurns"].executeTyped(selectedWandererEntity);
+    await tx.wait();
+    setIsBusy(false);
+  }, [systems, selectedWandererEntity]);
 
   return (
     <>
-      {isAvailable && (
-        // TODO replace with a working CustomButton+tooltip
-        <CustomButton args={[claimableTurns ?? ""]} disabled={isBusy} onClick={claimTurns} style={{ fontSize: "12px" }}>
-          {"claimTurns"}
-          <span className="text-dark-number">{` (${turns})`}</span>
-        </CustomButton>
-      )}
+      <CustomButton disabled={isBusy} onClick={claimTurns} style={{ fontSize: "12px" }}>
+        {"claimTurns"}
+        <span className="text-dark-number">{` (${claimableTurns})`}</span>
+      </CustomButton>
     </>
   );
 }
