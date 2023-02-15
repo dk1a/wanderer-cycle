3562daa14f81f041eaf93a2a01a54c779f84bc31
dk1a <dk1a@protonmail.com>
2023-02-16 00:51:32 +0300

    refactor: inline selectWandererEntity

--- packages/client/src/components/Wanderer/index.tsx
+++ packages/client/src/components/Wanderer/index.tsx
@@ -6,9 +6,6 @@ import { useWandererContext } from "../../contexts/WandererContext";
 
 export default function Wanderer({ wandererEntity }: { wandererEntity: EntityIndex }) {
   const { selectedWandererEntity, selectWandererEntity } = useWandererContext();
-  const selectWandererHandler = () => {
-    selectWandererEntity(wandererEntity);
-  };
 
   return (
     <div className={classes.wanderer}>
@@ -16,7 +13,7 @@ export default function Wanderer({ wandererEntity }: { wandererEntity: EntityInd
       {wandererEntity == selectedWandererEntity && <span className="text-dark-string">{" (selected)"}</span>}
       <WandererImage entity={wandererEntity} />
       <div className="">
-        <CustomButton onClick={selectWandererHandler}>Select</CustomButton>
+        <CustomButton onClick={() => selectWandererEntity(wandererEntity)}>Select</CustomButton>
       </div>
     </div>
   );
