dbbd10ec2340406c831b384dd1a40774cf171e35
dk1a <dk1a@protonmail.com>
2023-02-19 17:05:23 +0300

    fix old name

--- packages/client/src/components/Inventory/InventorySection.tsx
+++ packages/client/src/components/Inventory/InventorySection.tsx
@@ -1,11 +1,11 @@
-import InventoryLoot from "./InventoryEquipment";
+import InventoryEquipment from "./InventoryEquipment";
 import { useOwnedEquipment } from "../../mud/hooks/useOwnedEquipment";
 
 const InventorySection = ({ equipmentList }: { equipmentList: ReturnType<typeof useOwnedEquipment> }) => {
   return (
     <div className="flex items-center justify-center flex-wrap w-1/2">
       {equipmentList.map(({ entity }) => (
-        <InventoryLoot key={entity} entity={entity} />
+        <InventoryEquipment key={entity} entity={entity} />
       ))}
     </div>
   );
