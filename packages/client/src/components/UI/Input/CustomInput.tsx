d0dd40c22448ffea4b2ef15d2ba4358030742b35
rashid <gagach16rus@gmail.com>
2023-02-21 09:19:13 +0300

    stylization select

--- packages/client/src/components/UI/Input/CustomInput.tsx
+++ packages/client/src/components/UI/Input/CustomInput.tsx
@@ -6,7 +6,15 @@ type InputProps = {
   onChange: ChangeEventHandler;
 };
 const CustomInput = ({ onChange, value, placeholder }: InputProps) => {
-  return <input placeholder={placeholder} onChange={onChange} className="text-dark-400"></input>;
+  return (
+    <input
+      placeholder={placeholder}
+      onChange={onChange}
+      className="
+  bg-dark-500 border border-dark-400 text-dark-200 text-sm h-8 w-36 focus:border-dark-string block w-56 mx-2 p-2
+  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-dark-comment"
+    ></input>
+  );
 };
 
 export default CustomInput;
--- packages/client/src/components/UI/Select/CustomSelect.tsx
+++ packages/client/src/components/UI/Select/CustomSelect.tsx
@@ -1,7 +1,7 @@
 const CustomSelect = ({ option, defaultValue, value, onChange }: any) => {
   return (
     <select
-      className="bg-dark-400 border border-dark-comment text-dark-200 text-sm h-8 w-36 focus:border-dark-string block w-56 mx-2
+      className="bg-dark-500 border border-dark-400 text-dark-200 text-sm h-8 w-36 focus:border-dark-string block w-56 mx-2
     dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-dark-comment"
       value={value}
       onChange={(event) => onChange(event.target.value)}
