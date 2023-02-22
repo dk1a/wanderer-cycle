2106e05a8bc1e87a74529ac0ea6e06222c3b3cd0
rashid <gagach16rus@gmail.com>
2023-02-22 11:14:09 +0300

    base style in CurrentEquipment and moved InventoryProvider

--- packages/client/src/components/Inventory/CurrentEquipment.tsx
+++ packages/client/src/components/Inventory/CurrentEquipment.tsx
@@ -1,25 +1,32 @@
-import { useInventoryContext } from "../../contexts/InventoryContext";
+import Tippy from "@tippyjs/react";
+import CustomButton from "../UI/Button/CustomButton";
+import { left } from "@popperjs/core";
 
-const CurrentEquipment = () => {
-  const separator = <hr className="h-px my-2 bg-dark-400 border-0" />;
-  const locationName = null;
-
-  const { equipmentSlots } = useInventoryContext();
+const CurrentEquipment = ({ equipmentList, equipmentSlots }: any) => {
+  const affixes = equipmentList.map((i) => i.affixes);
+  console.log(equipmentSlots);
 
   return (
-    <section className="flex flex-col w-52 bg-dark-500 border border-dark-400 h-screen absolute top-16 right-0">
+    <section className="flex flex-col w-64 bg-dark-500 border border-dark-400 h-screen absolute top-16 right-0">
       <h4 className="col-span-3 text-center text-lg text-dark-type font-medium">Current Equipment</h4>
-      {/* TODO style this */}
       {equipmentSlots.map(({ entity, name, equipped, unequip }) => (
-        <div key={entity}>
-          <div>{name}</div>
-          {equipped !== undefined && (
-            <div>
-              <div>{equipped.name}</div>
-              <button onClick={() => unequip()}>unequip</button>
-            </div>
-          )}
-        </div>
+        <Tippy key={entity} placement={left} content={<span>Stats</span>}>
+          <div className="flex flex-wrap m-2 border border-dark-400 p-1.5">
+            <div className="text-dark-200">{name}:</div>
+            {equipped == undefined ? (
+              <>
+                <div>
+                  <div className="text-dark-type mx-1">{"equipped.name"}</div>
+                </div>
+                <CustomButton style={{ marginTop: "5px" }} onClick={() => unequip()}>
+                  unequip
+                </CustomButton>
+              </>
+            ) : (
+              <div className="text-dark-300 mx-2">empty</div>
+            )}
+          </div>
+        </Tippy>
       ))}
     </section>
   );
--- packages/client/src/components/Inventory/InventoryEquipment.tsx
+++ packages/client/src/components/Inventory/InventoryEquipment.tsx
@@ -1,30 +1,35 @@
 import { EquipmentData } from "../../contexts/InventoryContext";
 import { EffectModifier } from "../Effect/EffectStatmod";
+import CustomButton from "../UI/Button/CustomButton";
 
 const InventoryEquipment = ({ equipmentData }: { equipmentData: EquipmentData }) => {
   const affixes = equipmentData.affixes;
 
   // TODO affixes are more than effects, they need either a separate component or an extended EffectModifier
   return (
-    <div className="text-dark-key p-2 flex flex-col border border-dark-400 w-[220px] h-auto m-2">
+    <div className="text-dark-key p-2 flex flex-col justify-between border border-dark-400 w-[220px] h-auto m-2">
       <span className="text-lg text-dark-type flex-wrap flex box-border">{equipmentData.name}</span>
       <span className="">
         ilvl<span className="mx-2 text-dark-number">{equipmentData.ilvl}</span>
       </span>
-
-      {/* TODO style this */}
-      {equipmentData.availableSlots.map((slotData) => (
-        <button key={slotData.entity} onClick={() => slotData.equip()}>
-          equip
-          {equipmentData.availableSlots.length > 1 && <span> ({slotData.name})</span>}
-        </button>
-      ))}
 
       {affixes.map(({ protoEntity, value, partId, statmod }) => (
         <div className="flex box-content flex-wrap" key={`${partId}${protoEntity}`}>
           <EffectModifier protoEntity={statmod.protoEntity} value={value} />
         </div>
       ))}
+      <div className="flex justify-around mt-1">
+        {equipmentData.availableSlots.map((slotData) => (
+          <CustomButton
+            key={slotData.entity}
+            style={{ width: "80px", fontSize: "12px", padding: "0" }}
+            onClick={() => slotData.equip()}
+          >
+            equip
+            {equipmentData.availableSlots.length > 1 && <span className="text-dark-string"> ({slotData.name})</span>}
+          </CustomButton>
+        ))}
+      </div>
     </div>
   );
 };
