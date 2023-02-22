75bcec06b5a3d2b1f5ebb497ea469f9e02d6103c
rashid <gagach16rus@gmail.com>
2023-02-22 16:28:28 +0300

    BaseEquipmentDetails, data and styles

--- packages/client/src/components/Inventory/BaseEquipmentDetails.tsx
+++ packages/client/src/components/Inventory/BaseEquipmentDetails.tsx
@@ -1,6 +1,7 @@
 import { EffectStatmod } from "../Effect/EffectStatmod";
 import { LootAffix } from "../../mud/utils/getLootAffix";
 
+<<<<<<< HEAD
 type BaseEquipmentDetailsData = {
   affixes: LootAffix[];
   ilvl: number;
@@ -11,6 +12,15 @@ const BaseEquipmentDetails = ({ affixes, ilvl, name, className }: BaseEquipmentD
   return (
     <div className={className}>
       <span className="text-lg text-dark-type flex-wrap flex box-border">{name}</span>
+=======
+type BaseEquipmentDetails = {
+  affixes: LootAffix[];
+  ilvl: number;
+};
+const BaseEquipmentDetails = ({ affixes, ilvl }: BaseEquipmentDetails) => {
+  return (
+    <>
+>>>>>>> 83f854c (BaseEquipmentDetails, data and styles)
       <span className="text-dark-key">
         ilvl<span className="mx-2 text-dark-number">{ilvl}</span>
       </span>
@@ -19,7 +29,11 @@ const BaseEquipmentDetails = ({ affixes, ilvl, name, className }: BaseEquipmentD
           <EffectStatmod protoEntity={statmod.protoEntity} value={value} />
         </div>
       ))}
+<<<<<<< HEAD
     </div>
+=======
+    </>
+>>>>>>> 83f854c (BaseEquipmentDetails, data and styles)
   );
 };
 
