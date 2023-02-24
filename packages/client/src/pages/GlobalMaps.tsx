0666419fee32988795f1383a09105e38e36b25af
rashid <gagach16rus@gmail.com>
2023-02-24 10:21:59 +0300

    fix routing

--- packages/client/src/pages/GlobalMaps.tsx
+++ packages/client/src/pages/GlobalMaps.tsx
@@ -2,27 +2,16 @@ import Map from "../components/Map";
 import { useMaps } from "../mud/hooks/useMaps";
 import { useWandererContext } from "../contexts/WandererContext";
 import Combat from "../components/Combat";
-import Wanderer from "../components/Wanderer";
-import { useWandererEntities } from "../mud/hooks/useWandererEntities";
+import WandererSelect from "./WandererSelect";
 
 const GlobalMaps = () => {
   const mapEntities = useMaps("Global Basic");
   const { selectedWandererEntity, enemyEntity } = useWandererContext();
-  const wandererEntities = useWandererEntities();
 
   return (
     <>
       {selectedWandererEntity === undefined ? (
-        <div>
-          <h3 className="m-10 text-2xl font-bold text-dark-comment">{"// select a wanderer"}</h3>
-          <div className="flex justify-around">
-            <div className="flex flex-wrap gap-x-4 gap-y-4 mt-2 justify-around">
-              {wandererEntities.map((wandererEntity) => (
-                <Wanderer key={wandererEntity} wandererEntity={wandererEntity} />
-              ))}
-            </div>
-          </div>
-        </div>
+        <WandererSelect />
       ) : enemyEntity === undefined ? (
         <div className="flex justify-around flex-wrap">
           {mapEntities.map((entity) => (
--- packages/client/src/pages/SkillPage.tsx
+++ packages/client/src/pages/SkillPage.tsx
@@ -1,26 +1,15 @@
 import CycleInfo from "../components/info/CycleInfo";
 import SkillList from "../components/SkillList";
 import { useWandererContext } from "../contexts/WandererContext";
-import Wanderer from "../components/Wanderer";
-import { useWandererEntities } from "../mud/hooks/useWandererEntities";
+import WandererSelect from "./WandererSelect";
 
 const SkillPage = () => {
   const { selectedWandererEntity } = useWandererContext();
-  const wandererEntities = useWandererEntities();
 
   return (
     <section className="flex justify-center">
       {selectedWandererEntity === undefined ? (
-        <div>
-          <h3 className="m-10 text-2xl font-bold text-dark-comment">{"// select a wanderer"}</h3>
-          <div className="flex justify-around">
-            <div className="flex flex-wrap gap-x-4 gap-y-4 mt-2 justify-around">
-              {wandererEntities.map((wandererEntity) => (
-                <Wanderer key={wandererEntity} wandererEntity={wandererEntity} />
-              ))}
-            </div>
-          </div>
-        </div>
+        <WandererSelect />
       ) : (
         <div className="flex justify-center">
           <CycleInfo />
