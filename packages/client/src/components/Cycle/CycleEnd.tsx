bf698b26fb135b40fe0be9aac46dbf07424cf939
rashid <gagach16rus@gmail.com>
2023-03-07 16:22:40 +0300

    fix logic in useOnCompleteCycleEffect

--- packages/client/src/components/Cycle/CycleEnd.tsx
+++ packages/client/src/components/Cycle/CycleEnd.tsx
@@ -1,30 +1,30 @@
 import CustomButton from "../UI/Button/CustomButton";
 import { EntityIndex } from "@latticexyz/recs";
-import { useCompleteCycle, useOnCompleteCycleEffect } from "../../mud/hooks/cycle";
-import { useState } from "react";
+import { OnCompleteCycleData, useCompleteCycle, useOnCompleteCycleEffect } from "../../mud/hooks/cycle";
+import { ReactNode, SetStateAction, useState } from "react";
 import Modal from "../UI/Modal/Modal";
 
 export function CycleEnd({ wandererEntity }: { wandererEntity: EntityIndex; cycleEntity: EntityIndex }) {
   const [active, setActive] = useState(false);
+  const [identity, setIdentity] = useState();
   const CompleteCycle = useCompleteCycle(wandererEntity);
 
-  const onComplete = () => {
-    setActive(true);
-    CompleteCycle();
-  };
   useOnCompleteCycleEffect(wandererEntity, (identity) => {
-    console.log(`You gained ${identity} identity`);
+    setIdentity({ identity });
+    if (identity !== undefined) {
+      setActive(true);
+    }
   });
 
   return (
     <div className="flex flex-col items-center w-full">
       <div className="mt-10">
-        <CustomButton onClick={() => onComplete()}>Complete Cycle</CustomButton>
+        <CustomButton onClick={() => CompleteCycle()}>Complete Cycle</CustomButton>
       </div>
       <Modal active={active} setActive={setActive}>
         <div className="text-dark-string text-lg">
           You gained
-          <span className="text-dark-number mx-2">{"${identity}"}</span>
+          <span className="text-dark-number mx-2">{identity}</span>
           identity
         </div>
       </Modal>
