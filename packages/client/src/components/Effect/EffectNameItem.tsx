c8f6d7c2c7cde638a00e81ae8fb8ebceca52929f
rashid <gagach16rus@gmail.com>
2023-03-09 15:49:25 +0300

    add names for items

--- packages/client/src/components/Effect/EffectNameItem.tsx
+++ packages/client/src/components/Effect/EffectNameItem.tsx
@@ -1,16 +1,13 @@
 import { EntityIndex } from "@latticexyz/recs";
+import { useLoot } from "../../mud/hooks/useLoot";
 
 export default function EffectNameItem({ entity }: { entity: EntityIndex }) {
-  //const json = useUriJson(id)
-  // TODO get data from tokenURI
-  const json = { name: "itemPH" };
+  const loot = useLoot(entity);
 
   return (
     <>
       <span className="text-dark-key mr-1">item</span>
-      <span className="text-dark-method" title={json.name}>
-        {json.name}
-      </span>
+      <span className="text-dark-method">{loot.name}</span>
     </>
   );
 }
