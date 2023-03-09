60b82addc7c2687aefe5413236d0ef10652c3a8b
dk1a <dk1a@protonmail.com>
2023-03-09 12:56:23 +0300

    fix CombatActions

--- packages/client/src/components/Combat/Combat.tsx
+++ packages/client/src/components/Combat/Combat.tsx
@@ -1,30 +1,9 @@
-import { useCallback, useMemo, useState } from "react";
 import { useWandererContext } from "../../contexts/WandererContext";
-import { useExecuteCycleCombatRound } from "../../mud/hooks/combat";
-import { attackAction } from "../../mud/utils/combat";
-import { useSkills } from "../../mud/hooks/skill";
 import CombatActions from "./CombatActions";
 import { CombatRoundOutcome } from "./CombatRoundOutcome";
-import Select from "react-select";
-import "../UI/customSelect.scss";
-import CustomButton from "../UI/Button/CustomButton";
 
 export function Combat() {
-  const { selectedWandererEntity, lastCombatResult, learnedSkillEntities } = useWandererContext();
-  const executeCycleCombatRound = useExecuteCycleCombatRound();
-  const skills = useSkills(learnedSkillEntities);
-
-  const skillOptions = useMemo(() => skills.map(({ name, entity }) => ({ value: entity, label: name })), [skills]);
-  const [selectedSkill, selectSkill] = useState<(typeof skillOptions)[number] | null>(null);
-
-  const onAttack = useCallback(() => {
-    if (!selectedWandererEntity) {
-      throw new Error("Must select wanderer entity");
-    }
-    executeCycleCombatRound(selectedWandererEntity, [attackAction]);
-  }, [selectedWandererEntity, executeCycleCombatRound]);
-
-  // ========== RENDER ==========
+  const { lastCombatResult } = useWandererContext();
   return (
     <section className="p-2 flex flex-col justify-center items-center w-full">
       {/* TODO re-enable after implementing rounds
@@ -38,31 +17,8 @@ export function Combat() {
       <div className="flex justify-center w-1/2">
         <div className="text-2xl text-dark-type mr-2">selected map</div>
       </div>
-      {lastCombatResult !== undefined && (
-        <div className=" p-2 w-1/3 h-40 flex flex-col items-center mt-4">
-          <CombatRoundOutcome lastCombatResult={lastCombatResult} />
-          <CombatActions onAttack={onAttack} />
-        </div>
-      )}
-      {lastCombatResult == undefined && (
-        <div className="w-1/2 flex flex-col items-center mt-4">
-          <div className="flex items-center justify-around w-full">
-            <div>
-              <Select
-                classNamePrefix={"custom-select"}
-                placeholder={"select a skill"}
-                value={selectedSkill}
-                options={skillOptions}
-                onChange={selectSkill}
-              />
-            </div>
-            <div className="h-1/2">
-              <CustomButton>use skill</CustomButton>
-            </div>
-          </div>
-          <CombatActions onAttack={onAttack} />
-        </div>
-      )}
+      {lastCombatResult !== undefined && <CombatRoundOutcome lastCombatResult={lastCombatResult} />}
+      <CombatActions />
     </section>
   );
 }
--- packages/client/src/components/Combat/CombatActions.tsx
+++ packages/client/src/components/Combat/CombatActions.tsx
@@ -1,11 +1,60 @@
+import { useCallback, useMemo, useState } from "react";
+import Select from "react-select";
+import { useWandererContext } from "../../contexts/WandererContext";
+import { useExecuteCycleCombatRound } from "../../mud/hooks/combat";
+import { useSkills } from "../../mud/hooks/skill";
+import { attackAction } from "../../mud/utils/combat";
+import { SkillType } from "../../mud/utils/skill";
 import CustomButton from "../UI/Button/CustomButton";
+import "../UI/customSelect.scss";
 
-export default function CombatActions({ onAttack }: { onAttack: () => void }) {
+export default function CombatActions() {
+  const { selectedWandererEntity, learnedSkillEntities } = useWandererContext();
+  const [isBusy, setIsBusy] = useState(false);
+
+  const executeCycleCombatRound = useExecuteCycleCombatRound();
+  const onAttack = useCallback(async () => {
+    if (!selectedWandererEntity) throw new Error("Must select wanderer entity");
+    setIsBusy(true);
+    await executeCycleCombatRound(selectedWandererEntity, [attackAction]);
+    setIsBusy(false);
+  }, [selectedWandererEntity, executeCycleCombatRound]);
+
+  const skills = useSkills(learnedSkillEntities);
+  const combatSkills = useMemo(() => skills.filter(({ skillType }) => skillType === SkillType.COMBAT), [skills]);
+  const skillOptions = useMemo(
+    () => combatSkills.map(({ name, entity }) => ({ value: entity, label: name })),
+    [combatSkills]
+  );
+  const [selectedSkill, selectSkill] = useState<(typeof skillOptions)[number] | null>(null);
+  const onSkill = useCallback(() => {
+    if (!selectedWandererEntity) throw new Error("Must select wanderer entity");
+    setIsBusy(true);
+    console.log("TODO onSkill");
+    setIsBusy(false);
+  }, [selectedWandererEntity]);
+
   return (
-    <>
-      <div className="flex-default justify-center mt-8">
-        <CustomButton onClick={onAttack}>attack</CustomButton>
+    <div className="w-1/2 flex flex-col items-center mt-4">
+      <div className="flex items-center justify-around w-full">
+        <div>
+          <Select
+            classNamePrefix={"custom-select"}
+            placeholder={"select a skill"}
+            value={selectedSkill}
+            options={skillOptions}
+            onChange={selectSkill}
+          />
+        </div>
+        <div className="h-1/2">
+          <CustomButton onClick={onSkill} disabled={isBusy}>
+            use skill
+          </CustomButton>
+        </div>
       </div>
-    </>
+      <CustomButton onClick={onAttack} disabled={isBusy}>
+        attack
+      </CustomButton>
+    </div>
   );
 }
