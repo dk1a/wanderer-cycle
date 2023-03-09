a896705d08e8e74ec98a8d23ef4a8039a3f0aa81
rashid <gagach16rus@gmail.com>
2023-03-09 20:45:56 +0300

    basic logic nonCombat skill

--- packages/client/src/components/SkillList/SkillLearnable.tsx
+++ packages/client/src/components/SkillList/SkillLearnable.tsx
@@ -1,48 +1,71 @@
-import { EntityIndex } from "@latticexyz/recs";
-import { useSkill } from "../../mud/hooks/skill";
 import { useCallback, useMemo, useState } from "react";
+import Select from "react-select";
 import { useWandererContext } from "../../contexts/WandererContext";
-import Skill from "../Skill";
+import { useExecuteCycleCombatRound } from "../../mud/hooks/combat";
+import { useSkills } from "../../mud/hooks/skill";
+import { useMUD } from "../../mud/MUDContext";
+import { ActionType, attackAction, CombatAction } from "../../mud/utils/combat";
+import { SkillType } from "../../mud/utils/skill";
 import CustomButton from "../UI/Button/CustomButton";
-import { useLevel } from "../../mud/hooks/charstat";
-import { useActiveGuise, useGuise } from "../../mud/hooks/guise";
+import "../UI/customSelect.scss";
 
-export default function SkillLearnable({ entity }: { entity: EntityIndex }) {
-  const { learnCycleSkill, learnedSkillEntities, cycleEntity } = useWandererContext();
-  const skill = useSkill(entity);
+export default function CombatActions() {
+  const { world } = useMUD();
+  const { selectedWandererEntity, learnedSkillEntities } = useWandererContext();
+  const [isBusy, setIsBusy] = useState(false);
 
-  const isLearned = useMemo(() => learnedSkillEntities.includes(entity), [learnedSkillEntities, entity]);
+  // attack
+  const executeCycleCombatRound = useExecuteCycleCombatRound();
+  const onAttack = useCallback(async () => {
+    if (!selectedWandererEntity) throw new Error("Must select wanderer entity");
+    setIsBusy(true);
+    await executeCycleCombatRound(selectedWandererEntity, [attackAction]);
+    setIsBusy(false);
+  }, [selectedWandererEntity, executeCycleCombatRound]);
 
-  const [visible, setVisible] = useState(!isLearned);
-
-  const onHeaderClick = useCallback(() => {
-    // only expand collapsed data
-    if (!visible) {
-      setVisible(true);
-    }
-  }, [visible]);
-
-  const guise = useActiveGuise(cycleEntity);
-  const level = useLevel(cycleEntity, guise?.levelMul)?.level;
+  // skill list
+  const skills = useSkills(learnedSkillEntities);
+  const combatSkills = useMemo(() => skills.filter(({ skillType }) => skillType === SkillType.COMBAT), [skills]);
+  const skillOptions = useMemo(
+    () => combatSkills.map(({ name, entity }) => ({ value: entity, label: name })),
+    [combatSkills]
+  );
+  const [selectedSkill, selectSkill] = useState<(typeof skillOptions)[number] | null>(null);
+  // skill use callback
+  const onSkill = useCallback(async () => {
+    if (!selectedWandererEntity) throw new Error("Must select wanderer entity");
+    if (selectedSkill === null) throw new Error("No skill selected");
+    setIsBusy(true);
+    const skillEntityId = world.entities[selectedSkill.value];
+    const skillAction: CombatAction = {
+      actionType: ActionType.SKILL,
+      actionEntity: skillEntityId,
+    };
+    await executeCycleCombatRound(selectedWandererEntity, [skillAction]);
+    setIsBusy(false);
+  }, [world, selectedWandererEntity, executeCycleCombatRound, selectedSkill]);
 
   return (
-    <div className="p-0 flex items-center mb-8">
-      <Skill
-        skill={skill}
-        className={`bg-dark-500 border border-dark-400 p-2 w-[400px] ${isLearned ? "opacity-30" : ""}`}
-        isCollapsed={!visible}
-        onHeaderClick={onHeaderClick}
-      />
-      <div className="h-1/2 ml-10">
-        {!isLearned && (
-          <CustomButton
-            onClick={() => learnCycleSkill(entity)}
-            disabled={level !== undefined && level < skill.requiredLevel}
-          >
-            learn
+    <div className="w-1/2 flex flex-col items-center mt-4">
+      <div className="flex items-center justify-around w-full">
+        <div>
+          <Select
+            classNamePrefix={"custom-select"}
+            placeholder={"select a skill"}
+            value={selectedSkill}
+            options={skillOptions}
+            onChange={selectSkill}
+          />
+        </div>
+        <div className="h-1/2">
+          <CustomButton onClick={onSkill} disabled={isBusy}>
+            use skill
           </CustomButton>
-        )}
+        </div>
       </div>
+      <CustomButton onClick={onAttack} disabled={isBusy}>
+        attack
+      </CustomButton>
     </div>
   );
 }
