a250ad6d029dad9c05751f6194f41fff87c8492a
dk1a <dk1a@protonmail.com>
2023-03-14 14:25:09 +0300

    feat(client): testnet config and faucet drip

--- packages/client/src/mud/config.ts
+++ packages/client/src/mud/config.ts
@@ -3,21 +3,22 @@ import { getBurnerWallet } from "./getBurnerWallet";
 
 const params = new URLSearchParams(window.location.search);
 
-export const config: SetupContractConfig = {
+export const config: SetupContractConfig & { faucetServiceUrl?: string } = {
   clock: {
     period: 1000,
     initialTime: 0,
     syncInterval: 5000,
   },
   provider: {
-    jsonRpcUrl: params.get("rpc") ?? "http://localhost:8545",
-    wsRpcUrl: params.get("wsRpc") ?? "ws://localhost:8545",
-    chainId: Number(params.get("chainId")) || 31337,
+    jsonRpcUrl: params.get("rpc") ?? "https://follower.testnet-chain.linfra.xyz",
+    wsRpcUrl: params.get("wsRpc") ?? "wss://follower.testnet-chain.linfra.xyz",
+    chainId: Number(params.get("chainId")) || 4242,
   },
   privateKey: getBurnerWallet().privateKey,
-  chainId: Number(params.get("chainId")) || 31337,
-  snapshotServiceUrl: params.get("snapshot") ?? undefined,
-  initialBlockNumber: Number(params.get("initialBlockNumber")) || 0,
-  worldAddress: params.get("worldAddress") ?? "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-  devMode: true, //params.get("dev") === "true",
+  chainId: Number(params.get("chainId")) || 4242,
+  snapshotServiceUrl: params.get("snapshot") ?? "https://ecs-snapshot.testnet-mud-services.linfra.xyz",
+  faucetServiceUrl: params.get("faucet") ?? "https://faucet.testnet-mud-services.linfra.xyz",
+  initialBlockNumber: Number(params.get("initialBlockNumber")) || 9045311,
+  worldAddress: params.get("worldAddress") ?? "0xCceC5dFc845AdF8C4a1468A29E504059383503aF",
+  devMode: params.get("dev") === "true",
 };
--- packages/client/src/mud/setup.ts
+++ packages/client/src/mud/setup.ts
@@ -2,10 +2,11 @@ import { setupMUDNetwork } from "@latticexyz/std-client";
 import type { SystemTypes } from "contracts/types/SystemTypes";
 import { SystemAbis } from "contracts/types/SystemAbis.mjs";
 import type { EntityID } from "@latticexyz/recs";
-import { SingletonID } from "@latticexyz/network";
+import { createFaucetService, SingletonID } from "@latticexyz/network";
 import { config } from "./config";
 import { components, clientComponents } from "./components";
 import { world } from "./world";
+import { ethers } from "ethers";
 
 export type SetupResult = Awaited<ReturnType<typeof setup>>;
 
@@ -27,6 +28,28 @@ export const setup = async () => {
 
   // Add support for optimistic rendering
   const componentsWithOverrides = {};
+
+  // Request drip from faucet
+  if (!config.devMode && config.faucetServiceUrl) {
+    const faucet = createFaucetService(config.faucetServiceUrl);
+    console.info("[Dev Faucet]: Player Address -> ", address);
+
+    const requestDrip = async () => {
+      const balance = await result.network.signer.get()?.getBalance();
+      console.info(`[Dev Faucet]: Player Balance -> ${balance}`);
+      const playerIsBroke = balance?.lte(ethers.utils.parseEther("1"));
+      console.info(`[Dev Faucet]: Player is broke -> ${playerIsBroke}`);
+      if (playerIsBroke) {
+        console.info("[Dev Faucet]: Dripping funds to player");
+        // Double drip
+        address && (await faucet?.dripDev({ address })) && (await faucet?.dripDev({ address }));
+      }
+    };
+
+    requestDrip();
+    // Request a drip every 60 seconds
+    setInterval(requestDrip, 60000);
+  }
 
   return {
     ...result,
