1ba099cefe61f333a088248d8768698a778315fb
rashid <gagach16rus@gmail.com>
2023-03-15 10:42:05 +0300

    add copy adress

---
+++ packages/client/src/components/UI/CopyAndCopied/CopyAndCopied.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import copyDark from "../../img/copy.png";
+import copyLight from "../../img/copyLight.png";
+
+export default function CopyAndCopied({ textData }: { textData: string }) {
+  const [copied, setCopied] = useState(false);
+  const copyToClipBoard = () => {
+    setCopied(true);
+    navigator.clipboard.writeText(textData).then(
+      () => {
+        console.log("copied");
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+    setTimeout(() => {
+      setCopied(false);
+    }, 100);
+  };
+
+  return (
+    <div className="flex justify-center ml-4">
+      {!copied ? (
+        <img src={copyDark} alt="copy" onClick={copyToClipBoard} className="cursor-pointer w-4 h-4" />
+      ) : (
+        <img src={copyLight} alt="copy" onClick={copyToClipBoard} className="cursor-pointer w-4 h-4" />
+      )}
+    </div>
+  );
+}
--- packages/client/src/components/info/BaseInfo.tsx
+++ packages/client/src/components/info/BaseInfo.tsx
@@ -1,4 +1,4 @@
-import { EntityIndex } from "@latticexyz/recs";
+import { EntityID, EntityIndex } from "@latticexyz/recs";
 import { Fragment, ReactNode } from "react";
 import { LevelData, useLife, useMana, usePstats } from "../../mud/hooks/charstat";
 import { useAppliedEffects } from "../../mud/hooks/useAppliedEffects";
@@ -6,16 +6,18 @@ import { useLifeCurrent, useManaCurrent, useIdentityCurrent } from "../../mud/ho
 import { ElementalStatmods } from "../ElementalStatmods";
 import { PStatWithProgress } from "./PStatWithProgress";
 import EffectList from "../EffectList";
+import CopyAndCopied from "../UI/CopyAndCopied/CopyAndCopied";
 
 export interface BaseInfoProps {
   entity: EntityIndex | undefined;
   name: string | undefined;
+  entityId?: EntityID;
   locationName: string | null | undefined;
   levelData: LevelData;
   turnsHtml?: ReactNode;
 }
 
-export default function BaseInfo({ entity, name, locationName, levelData, turnsHtml }: BaseInfoProps) {
+export default function BaseInfo({ entity, entityId, name, locationName, levelData, turnsHtml }: BaseInfoProps) {
   const pstats = usePstats(entity);
 
   const life = useLife(entity);
@@ -44,16 +46,20 @@ export default function BaseInfo({ entity, name, locationName, levelData, turnsH
 
   return (
     <section className="flex flex-col w-64 bg-dark-500 border border-dark-400 h-full">
-      <h4 className="relative col-span-3 text-center text-lg text-dark-type font-medium">{name}</h4>
-      {locationName !== null && <div className="col-span-3 text-center text-dark-string">{locationName}</div>}
-
+      <div className="flex flex-col items-center">
+        <h4 className="relative col-span-3 text-center text-lg text-dark-type font-medium">{name}</h4>
+        <div className="flex justify-center items-center">
+          <span className="text-[9px] text-center break-all text-dark-key w-2/3">{entityId}</span>
+          <CopyAndCopied textData={entityId} />
+        </div>
+        {locationName !== null && <div className="col-span-3 text-center text-dark-string">{locationName}</div>}
+      </div>
       {identityCurrent !== undefined && (
         <span className="w-36 text-dark-key ml-1">
           {"identity: "}
           <span className="text-dark-number">{identityCurrent}</span>
         </span>
       )}
-
       <PStatWithProgress name={"level"} baseLevel={levelData?.level} experience={levelData?.experience} />
       {separator}
       {pstats.map((pstat) => (
