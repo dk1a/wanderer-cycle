8de04b4fbe91ed467b3f8c077724843661de45a0
rashid <gagach16rus@gmail.com>
2023-03-16 18:05:04 +0300

    add react toastify

--- packages/client/src/components/Inventory/BaseEquipment.tsx
+++ packages/client/src/components/Inventory/BaseEquipment.tsx
@@ -1,6 +1,7 @@
 import { EquipmentData } from "../../contexts/InventoryContext";
 import { EffectStatmod } from "../Effect/EffectStatmod";
 import CustomButton from "../UI/Button/CustomButton";
+import { toast } from "react-toastify";
 
 type BaseEquipmentProps = {
   equipmentData: EquipmentData;
@@ -10,6 +11,30 @@ type BaseEquipmentProps = {
 export default function BaseEquipment({ equipmentData, className }: BaseEquipmentProps) {
   const { name, ilvl, affixes } = equipmentData;
   const availableSlots = equipmentData.availableSlots;
+
+  console.log("data", equipmentData);
+
+  const handler = () => {
+    const toastId = toast.loading("taking....");
+    const status = equipmentData.equippedToSlot;
+    if (status === undefined) {
+      toast.update(toastId, {
+        isLoading: false,
+        type: "error",
+        render: ``,
+        autoClose: 5000,
+        closeButton: true,
+      });
+    } else if (!status) {
+      toast.update(toastId, {
+        isLoading: true,
+        type: "success",
+        render: `equipped`,
+        autoClose: 5000,
+        closeButton: true,
+      });
+    }
+  };
   return (
     <div className="text-dark-key p-1.5 flex flex-col justify-between border border-dark-400 bg-dark-500 w-64 m-2">
       <div className={className}>
@@ -36,10 +61,23 @@ export default function BaseEquipment({ equipmentData, className }: BaseEquipmen
             <CustomButton
               key={slotData.entity}
               style={{ width: "80px", fontSize: "12px", padding: "5px", border: "none", marginTop: "5px" }}
-              onClick={() => slotData.equip()}
+              onClick={() => {
+                const toastId = toast.loading("taking....");
+                const status = equipmentData.equippedToSlot;
+                if (!status) {
+                  toast.update(toastId, {
+                    isLoading: false,
+                    type: "success",
+                    render: `equipped`,
+                    autoClose: 1,
+                    closeButton: true,
+                  });
+                }
+                slotData.equip();
+              }}
             >
               equip
-              {availableSlots.length > 1 && <span className="text-dark-string"> {slotData.name}</span>}
+              {availableSlots.length > 1 && <span className="text-dark-string">{slotData.name}</span>}
             </CustomButton>
           ))}
         </div>
