0066272025568243012c3419b588955f1e95949d
rashid <gagach16rus@gmail.com>
2023-03-21 10:02:35 +0300

    refactor state to selectedWanderer

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,6 +2,9 @@ import { SyncState } from "@latticexyz/network";
 import { useComponentValue } from "@latticexyz/react";
 import { useMUD } from "./mud/MUDContext";
 import { AppRouter } from "./AppRouter";
+import { WandererProvider } from "./contexts/WandererContext";
+import { useEffect, useState } from "react";
+import { EntityIndex } from "@latticexyz/recs";
 
 export const App = () => {
   const {
@@ -15,19 +18,23 @@ export const App = () => {
     percentage: 0,
   });
 
+  const [selectedWandererEntity, selectWandererEntity] = useState<EntityIndex>();
+
   return (
-    <div>
-      {loadingState.state !== SyncState.LIVE ? (
-        <div className="flex w-full items-center justify-center mt-10">
-          <div className="text-center text-xl">
-            {loadingState.msg} {"("}
-            <span className="text-dark-number">{Math.floor(loadingState.percentage)}</span>
-            {"%)"}
+    <WandererProvider selectedWandererEntity={selectedWandererEntity} selectWandererEntity={selectWandererEntity}>
+      <div>
+        {loadingState.state !== SyncState.LIVE ? (
+          <div className="flex w-full items-center justify-center mt-10">
+            <div className="text-center text-xl">
+              {loadingState.msg} {"("}
+              <span className="text-dark-number">{Math.floor(loadingState.percentage)}</span>
+              {"%)"}
+            </div>
           </div>
-        </div>
-      ) : (
-        <AppRouter />
-      )}
-    </div>
+        ) : (
+          <AppRouter />
+        )}
+      </div>
+    </WandererProvider>
   );
 };
--- packages/client/src/contexts/WandererContext.tsx
+++ packages/client/src/contexts/WandererContext.tsx
@@ -13,7 +13,7 @@ import { useLearnedSkillEntities } from "../mud/hooks/skill";
 import { useMUD } from "../mud/MUDContext";
 
 type WandererContextType = {
-  selectedWandererEntity?: EntityIndex;
+  selectedWandererEntity: EntityIndex;
   selectWandererEntity: (wanderer: EntityIndex | undefined) => void;
   cycleEntity?: EntityIndex;
   previousCycleEntity?: EntityIndex;
@@ -27,13 +27,18 @@ type WandererContextType = {
   toggleWandererMode: () => void;
 };
 
+type WandererProviderType = {
+  children: ReactNode;
+  selectWandererEntity: (wanderer: EntityIndex | undefined) => void;
+  selectedWandererEntity: EntityIndex | undefined;
+};
+
 const WandererContext = createContext<WandererContextType | undefined>(undefined);
 
-export const WandererProvider = (props: { children: ReactNode }) => {
+export const WandererProvider = ({ children, selectWandererEntity, selectedWandererEntity }: WandererProviderType) => {
   const currentValue = useContext(WandererContext);
   if (currentValue) throw new Error("WandererProvider can only be used once");
 
-  const [selectedWandererEntity, selectWandererEntity] = useState<EntityIndex>();
   const {
     world,
     components: { ActiveCycle, ActiveCyclePrevious },
@@ -77,7 +82,7 @@ export const WandererProvider = (props: { children: ReactNode }) => {
     wandererMode,
     toggleWandererMode,
   };
-  return <WandererContext.Provider value={value}>{props.children}</WandererContext.Provider>;
+  return <WandererContext.Provider value={value}>{children}</WandererContext.Provider>;
 };
 
 export const useWandererContext = () => {
