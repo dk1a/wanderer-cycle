77de42ff1da1d3cee3a0dfb13f41b94de22e272f
rashid <gagach16rus@gmail.com>
2023-04-06 16:34:07 +0300

    add imports

--- packages/contracts/src/guise/test/LibGuiseLevel.t.sol
+++ packages/contracts/src/guise/test/LibGuiseLevel.t.sol
@@ -5,60 +5,40 @@ pragma solidity ^0.8.17;
 import { BaseTest } from "../../BaseTest.sol";
 
 import { IUint256Component } from "solecs/interfaces/IUint256Component.sol";
+import { getAddressById } from "solecs/utils.sol";
 
 import "forge-std/console.sol";
 
 import { LibExperience } from "../../charstat/LibExperience.sol";
 import { LibGuiseLevel } from "../LibGuiseLevel.sol";
+
 import { ActiveGuiseComponent, ID as ActiveGuiseComponentID } from "../ActiveGuiseComponent.sol";
 import { GuisePrototypeComponent, ID as GuisePrototypeComponentID } from "../GuisePrototypeComponent.sol";
+
 import { getGuiseProtoEntity } from "../GuisePrototypeComponent.sol";
 import { getSkillProtoEntity } from "../../skill/SkillPrototypeComponent.sol";
-import { LibSkill } from "../../skill/LibSkill.sol";
-import { LibLearnedSkills } from "../../skill/LibLearnedSkills.sol";
 
 contract LibGuiseLevelTest is BaseTest {
   using LibExperience for LibExperience.Self;
-  using LibSkill for LibSkill.Self;
-  using LibLearnedSkills for LibLearnedSkills.Self;
 
-  LibLearnedSkills.Self learnedSkills;
   // entities
   uint256 userEntity = uint256(keccak256("userEntity"));
   uint256 otherEntity = uint256(keccak256("otherEntity"));
 
   // guise
-  uint256 guise = getGuiseProtoEntity("Warrior");
+  uint256 guiseEntity = getGuiseProtoEntity("Warrior");
 
-  uint256 cleavePE = getSkillProtoEntity("Cleave");
-  uint256 chargePE = getSkillProtoEntity("Charge");
-  uint256 parryPE = getSkillProtoEntity("Parry");
-  uint256 someInvalidSkillPE = getSkillProtoEntity("someInvalidSkill");
+  ActiveGuiseComponent activeGuiseComp = ActiveGuiseComponent(getAddressById(components, ActiveGuiseComponentID));
+  GuisePrototypeComponent guiseProto = GuisePrototypeComponent(getAddressById(components, GuisePrototypeComponentID));
 
   function setUp() public virtual override {
     super.setUp();
-    learnedSkills = LibLearnedSkills.__construct(world, userEntity);
 
-    // give user some mana
-    // allow user to receive experience
-
-    // learn sample skills
-    learnedSkills.learnSkill(cleavePE);
-    learnedSkills.learnSkill(chargePE);
-    learnedSkills.learnSkill(parryPE);
-
-    console.log("guise guise guise");
+    uint256 guiseProtoEntity = activeGuiseComp.getValue(guiseProtoEntity);
+    uint32 levelMul = guiseProto.getValue(guiseProtoEntity).levelMul;
   }
 
   function test_guise() public {}
 
-  function testSetup() public {
-    assertTrue(learnedSkills.hasSkill(cleavePE));
-    assertTrue(learnedSkills.hasSkill(chargePE));
-    assertTrue(learnedSkills.hasSkill(parryPE));
-  }
-
-  function test_hasSkill_invalidSkill() public {
-    assertFalse(learnedSkills.hasSkill(someInvalidSkillPE));
-  }
+  function testSetup() public {}
 }
