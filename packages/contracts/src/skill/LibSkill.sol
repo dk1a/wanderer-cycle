c19152c35bb77a680b68bba8cccaee36b91921f0
R-Morpheus <gagach16rus@gmail.com>
2024-02-26 13:24:07 +0300

    update LibSkill

--- packages/contracts/src/skill/LibSkill.sol
+++ packages/contracts/src/skill/LibSkill.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
-//import { EffectSubSystem, ID as EffectSubSystemID } from "../effect/EffectSubSystem.sol";
 import { LibCharstat } from "../charstat/LibCharstat.sol";
 import { LibLearnedSkills } from "./LibLearnedSkills.sol";
-import { SkillTemplate, SkillTemplateTableId, SkillTemplateData, LearnedSkills, EffectDuration, EffectTemplate, DurationIdxMap, ManaCurrent, LifeCurrent } from "../codegen/tables/StatmodBase.sol";
+import { SkillTemplate, SkillTemplateTableId, SkillTemplateData, LearnedSkills, EffectDuration, EffectTemplate, DurationIdxMap, ManaCurrent, LifeCurrent, GenericDurationData } from "../codegen/index.sol";
+import { LibEffect } from "../modules/effect/LibEffect.sol";
+import { SkillType, TargetType } from "../codegen/common.sol";
+import { LibEffectPrototype } from "../effect/LibEffectPrototype.sol";
+import { Duration } from "../modules/duration/Duration.sol";
 
 library LibSkill {
   error LibSkill__SkillMustBeLearned();
@@ -29,37 +32,42 @@ library LibSkill {
   /**
    * @dev Change Self to use a different skill prototype
    */
-  function switchSkill(bytes32 skillEntity) internal view returns (SkillTemplateData skill) {
+  function switchSkill(bytes32 skillEntity) internal view returns (SkillTemplateData memory skill) {
     skill = SkillTemplate.get(skillEntity);
     return skill;
   }
 
-  //  function requireCombat(bytes32 skillEntity) internal pure {
-  //    if (skill.skillType != SkillType.COMBAT) {
-  //      revert LibSkill__RequiredCombat();
-  //    }
-  //  }
+  function requireCombat(bytes32 skillEntity) internal pure {
+    if (SkillTemplate.getSkillType(skillEntity) != SkillType.COMBAT) {
+      revert LibSkill__RequiredCombat();
+    }
+  }
 
-  //  function requireNonCombat(bytes32 skillEntity) internal pure {
-  //    if (skill.skillType == SkillType.COMBAT) {
-  //      revert LibSkill__RequiredNonCombat();
-  //    }
-  //  }
+  function requireNonCombat(bytes32 skillEntity) internal pure {
+    if (SkillTemplate.getSkillType(skillEntity) == SkillType.COMBAT) {
+      revert LibSkill__RequiredNonCombat();
+    }
+  }
 
   /**
    * @dev Combat skills may target either self or enemy, depending on skill prototype
    */
-  //  function chooseCombatTarget(bytes32 enemyEntity, bytes32 userEntity) internal pure returns (uint256) {
-  //    if (skill.effectTarget == TargetType.SELF || skill.effectTarget == TargetType.SELF_OR_ALLY) {
-  //      // self
-  //      return userEntity;
-  //    } else if (skill.effectTarget == TargetType.ENEMY) {
-  //      // enemy
-  //      return enemyEntity;
-  //    } else {
-  //      revert LibSkill__InvalidSkillTarget();
-  //    }
-  //  }
+  function chooseCombatTarget(
+    bytes32 enemyEntity,
+    bytes32 userEntity,
+    bytes32 skillEntity
+  ) internal pure returns (bytes32) {
+    TargetType targetType = SkillTemplate.getTargetType(skillEntity);
+    if (targetType == TargetType.SELF || targetType == TargetType.SELF_OR_ALLY) {
+      // self
+      return userEntity;
+    } else if (targetType == TargetType.ENEMY) {
+      // enemy
+      return enemyEntity;
+    } else {
+      revert LibSkill__InvalidSkillTarget();
+    }
+  }
 
   /**
    * @dev Check some requirements, subtract cost, start cooldown, apply effect.
@@ -81,9 +89,9 @@ library LibSkill {
       revert LibSkill__SkillOnCooldown();
     }
     // verify self-only skill
-    //    if (__self.skill.effectTarget == TargetType.SELF && __self.userEntity != targetEntity) {
-    //      revert LibSkill__InvalidSkillTarget();
-    //    }
+    if (SkillTemplate.getTargetType(skillEntity) == TargetType.SELF & userEntity != targetEntity) {
+      revert LibSkill__InvalidSkillTarget();
+    }
     // TODO verify other target types?
 
     // start cooldown
@@ -103,28 +111,29 @@ library LibSkill {
     if (cost > manaCurrent) {
       revert LibSkill__NotEnoughMana();
     } else if (cost > 0) {
-      LibCharstat.setManaCurrent(manaCurrent - cost);
+      LibCharstat.setManaCurrent(skillEntity, manaCurrent - cost);
     }
 
-    //    _applySkillEffect(__self, targetEntity);
+    _applySkillEffect(targetEntity, skillEntity);
   }
 
-  //  function _applySkillEffect(bytes32 targetEntity, bytes32 skillEntity) private {
-  //    if (!effectSubSystem.isEffectPrototype(skillEntity)) {
-  //      // skip if skill has no effect
-  //      return;
-  //    }
-  //
-  //    if (skill.skillType == SkillType.PASSIVE) {
-  //      // toggle passive skill
-  //      if (effectSubSystem.has(targetEntity, skillEntity)) {
-  //        effectSubSystem.executeRemove(targetEntity, skillEntity);
-  //      } else {
-  //        effectSubSystem.executeApply(targetEntity, skillEntity);
-  //      }
-  //    } else {
-  //      // apply active skill
-  //      effectSubSystem.executeApplyTimed(targetEntity, skillEntity, skill.duration);
-  //    }
-  //  }
+  function _applySkillEffect(bytes32 targetEntity, bytes32 skillEntity) private {
+    if (!LibEffect.hasEffectTemplate(skillEntity)) {
+      // skip if skill has no effect
+      return;
+    }
+
+    if (SkillTemplate.getTargetType(skillEntity) == SkillType.PASSIVE) {
+      // toggle passive skill
+      if (LibEffect.hasEffectApplied(targetEntity, skillEntity)) {
+        LibEffect.remove(targetEntity, skillEntity);
+      } else {
+        LibEffect.applyEffect(targetEntity, skillEntity);
+      }
+    } else {
+      // apply active skill
+      GenericDurationData memory duration = Duration.get(SkillTemplateTableId, targetEntity, skillEntity);
+      LibEffect.applyTimedEffect(targetEntity, skillEntity, duration);
+    }
+  }
 }
