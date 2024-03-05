8749cc6f753086134734c29b96c5e0ba9014bec1
dk1a <dk1a@protonmail.com>
2024-03-05 18:26:27 +0300

    fixes

--- packages/contracts/src/wanderer/WandererSpawnSystem.sol
+++ packages/contracts/src/wanderer/WandererSpawnSystem.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.21;
 
+import { System } from "@latticexyz/world/src/System.sol";
 import { getUniqueEntity } from "@latticexyz/world-modules/src/modules/uniqueentity/getUniqueEntity.sol";
 
 import { DefaultWheel, Wanderer, GuisePrototype } from "../codegen/index.sol";
@@ -9,7 +10,7 @@ import { LibCycle } from "../cycle/LibCycle.sol";
 
 /// @title Spawn a wandererEntity and start a cycle for it.
 /// @dev This is for new players, whereas StartCycle is for existing ones.
-contract WandererSpawnSystem {
+contract WandererSpawnSystem is System {
   error WandererSpawn_InvalidGuise();
 
   /// @notice Anyone can freely spawn wanderers, a wanderer is a tokenized game account
